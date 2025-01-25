b03fef796b4d8cac11b9f8198923c72a23088fab
darsh609 <116168977+darsh609@users.noreply.github.com>
2025-01-26 01:55:38 +0530

    done

--- src/Updates/Update.jsx
+++ src/Updates/Update.jsx
@@ -37,7 +37,9 @@ const UpdateSection = () => {
   const fetchUpdates = async () => {
     try {
       const response = await axios.get(`${BASE_URL}/getupdate`);
+
       setUpdates(response.data);
+      
     } catch (error) {
       console.error("Error fetching updates:", error);
     }
